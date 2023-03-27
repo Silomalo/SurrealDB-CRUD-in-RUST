5677482cf97a2f984ae986aacf7bcacf8494497b
silomalo <silomalojoseph@gmail.com>
2023-03-28 00:36:22 +0300

    feat: In-memory database implementation (Stored in RAM)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::collections::BTreeMap;
 use surrealdb::sql::{thing, Datetime, Object, Thing, Value};
 use surrealdb::{Datastore, Response,Session};
 
+type 
+
 #[tokio::main]
 async fn main()->Result<()>{
     // let ds = Datastore::new("http://localhost:8080", "admin", "admin").await?;
@@ -11,7 +13,14 @@ async fn main()->Result<()>{
     let ses =Session::for_db("my_ns","my_db");
 
     //ccreate a table
-    let sql ="CREATE task:1 SET title = 'Task 01', priority=10";
+    let sql ="CREATE task SET title = 'Task 01', priority=10";
+    let res = ds.execute(sql, &ses, None, false).await?;
+    let sql ="CREATE task SET title = 'Task 02', priority=20";
+    let res = ds.execute(sql, &ses, None, false).await?;
+    // println!("{:?}", res);
+
+    //get all records from table
+    let sql ="SELECT * FROM task";
     let res = ds.execute(sql, &ses, None, false).await?;
     println!("{:?}", res);
 
